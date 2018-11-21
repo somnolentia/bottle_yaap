874cfaf68ae03e6d6adf4cacdc5235d50c63cfab
Pieter Vermeylen <pieter.vermeylen@luminix.fi>
2018-11-21 09:22:31 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup
+
+version = "2.0.0"
+project = 'lx_tm'
+author = "Pieter Vermeylen"
+author_email = "pieter@somnolentia.net"
+license = "MIT"
+
+
+def _read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+def get_reqs():
+    with open('reqs.txt') as fp:
+        return fp.read().splitlines()
+
+
+setup(
+    name=project,
+    version=version,
+    license=license,
+    description="luminix translation memory module",
+    long_description=_read('README.rst'),
+    platforms=('Any'),
+    keywords="",
+    author=author,
+    author_email=author_email,
+    url=f'https://www.luminix.fi/code/{project}',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+
+    py_modules=[project],
+    install_requires=get_reqs(),
+    entry_points={
+        'console_scripts': ['tm=lx_tm:cli'],
+    }
+)
