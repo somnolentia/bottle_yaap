c16b269cd2916a85a261ecf193be0b78137171a7
Pieter Vermeylen <pieter.vermeylen@luminix.fi>
2021-06-16 09:24:20 +0300

    Testing combining parameters from two sources

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,19 +1,31 @@
+//def somelibrary = evaluate readTrusted('JenkinsLibrary.groovy') 
+def params_a = [
+    string(name: 'VERSION')
+]
+def params_b = [
+    string(name: 'APP')
+]
 
+def method_setup_parameters(){
+    def combined_params = params_a + params_b
+    properties([parameters(combined_params)])
+}
 pipeline {
     agent any
 
-    parameters {
-        persistentString(
-            name: 'VERSION',
-            defaultValue: 'Some value',
-            description: 'Provide the version number',
-            successfulOnly: false)
-        string(
-            name: 'NOPERSIST',
-            defaultValue: 'no idea',
-            description: 'Will not persist'
-        )
-    }
+    method_setup_parameters()
+    //parameters {
+    //    persistentString(
+    //        name: 'VERSION',
+    //        defaultValue: 'Some value',
+    //        description: 'Provide the version number',
+    //        successfulOnly: false)
+    //    string(
+    //        name: 'NOPERSIST',
+    //        defaultValue: 'no idea',
+    //        description: 'Will not persist'
+    //    )
+    //}
 
     stages {
 
